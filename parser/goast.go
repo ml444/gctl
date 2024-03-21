c6134828b4be3d9d2fedca8bcea2e107eb1d4941
bryce <you@example.com>
2024-03-21 22:02:25 +0800

    fix:ok

--- parser/goast.go
+++ parser/goast.go
@@ -100,3 +100,95 @@ func (pd *CtxData) parseServiceMethod(astFile *ast.File) {
 	}
 	pd.ServiceMethodMap = svcMap
 }
+
+type GoFileAST struct {
+	Consts  map[string]struct{}
+	Objects map[string]string
+	Funcs   map[string]struct{}
+	Structs map[string]*StructAST
+}
+
+type StructAST struct {
+	StructName string
+	Fields     map[string]string
+	Methods    map[string]*MethodAST
+}
+
+type MethodAST struct {
+	Name    string
+	ReqArgs []string
+	RspArgs []string
+}
+
+func ParseFile(filePath string) (*GoFileAST, error) {
+	src, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+	tf := token.NewFileSet()
+	astFile, err := parser.ParseFile(tf, filePath, src, parser.ParseComments)
+	if err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+	data := GoFileAST{
+		Consts:  map[string]struct{}{},
+		Objects: map[string]string{},
+		Funcs:   map[string]struct{}{},
+		Structs: map[string]*StructAST{},
+	}
+
+	{
+		for k, obj := range astFile.Scope.Objects {
+			data.Objects[k] = obj.Kind.String()
+		}
+	}
+	for _, decl := range astFile.Decls {
+		switch decl.(type) {
+		case *ast.GenDecl:
+			// TODO
+		case *ast.FuncDecl:
+			v := decl.(*ast.FuncDecl)
+			if v.Recv != nil {
+				// NOTE: this is method function
+
+				// svcName := v.Recv.List[0].Type.(*ast.Ident).Name
+				// svcName := v.Recv.List[0].Type.(*ast.StarExpr).X.(*ast.Ident).Name
+				ident, ok := v.Recv.List[0].Type.(*ast.Ident)
+				if !ok {
+					continue
+				}
+				svcName := ident.Name
+				methodName := v.Name.Name
+				if methodName[0] >= 'A' && methodName[0] <= 'Z' {
+					st, ok := data.Structs[svcName]
+					if !ok {
+						st = &StructAST{
+							StructName: svcName,
+						}
+						data.Structs[svcName] = st
+					}
+					method := &MethodAST{
+						Name: methodName,
+					}
+					for _, arg := range v.Type.Params.List {
+						reqExpr := arg.Type.(*ast.StarExpr).X.(*ast.SelectorExpr)
+						req := strings.Join([]string{reqExpr.X.(*ast.Ident).Name, reqExpr.Sel.Name}, ".")
+						method.ReqArgs = append(method.ReqArgs, req)
+					}
+					for _, arg := range v.Type.Results.List {
+						rspExpr := arg.Type.(*ast.StarExpr).X.(*ast.SelectorExpr)
+						rsp := strings.Join([]string{rspExpr.X.(*ast.Ident).Name, rspExpr.Sel.Name}, ".")
+						method.RspArgs = append(method.RspArgs, rsp)
+					}
+					st.Methods[methodName] = method
+				}
+			} else {
+				// normal function
+				data.Funcs[v.Name.Name] = struct{}{}
+			}
+		}
+	}
+	return &data, nil
+}
