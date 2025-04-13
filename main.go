e6625abcf540d129b95b3a62b1910e4a14ce3696
hubraw <githubraw@gmail.com>
2025-04-14 00:18:33 +0800

    fix: ...

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
-)
+import "github.com/ml444/gctl/cmd"
 
 func main() {
-	// cmd.Execute()
-	cur, err := os.Getwd()
-	if err != nil {
-		fmt.Print(err.Error())
-		return
-	}
-	fmt.Print("===curl>:", cur)
-	dir, file := filepath.Split(cur)
-	fmt.Printf("===> dir:%s, file: %s\n", dir, file)
-	names := strings.Split(cur, string(os.PathSeparator))
-	fmt.Printf("===> names: %#v, len: %d\n", names, len(names))
+	cmd.Execute()
 }
